05cec15e9f3fd45301a989d88abd92ba70450317
Jamar Torres <Jetorres88@gmail.com>
2020-01-03 17:23:59 -0500

    added subclasses and queried data

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Note, Folders
-# Register your models here.
-admin.site.register(Note)
+from .models import Note, PersonalNote, Folders, SubFolders
 
-admin.site.register(Folders)
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+# Register your models here.
+admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote)
+
+admin.site.register(Folders, NoteAdmin)
+admin.site.register(SubFolders)
--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 # this uses all the built in model classes from django.
 class Note(models.Model):
@@ -9,8 +11,22 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
 class Folders(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     list_notes = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class SubFolders(Folders):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
